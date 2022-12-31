9112e8fc8dd6cd4be2d6fd406cfb6160d1de69fa
Bungfro <sythmn@gmail.com>
2022-12-31 04:00:02 +0000

    Add account and channel base

--- src/collections/Account.ts
+++ src/collections/Account.ts
@@ -1,11 +1,12 @@
-import { Collection } from 'fireorm';
-import { IsEmail } from 'class-validator';
+lsimport { Collection, SubCollecti, ISubCollectionon } from 'fireorm';
+import { Channel } from './Channel'
 
 @Collection()
 export class Todo {
   id: string;
   username: string;
-  
-  @IsEmail()
   email: string;
+  
+  @SubCollection(Channel)
+  channs?: ISubCollection<Channel>
 }
